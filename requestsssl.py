7d4559b7bb835868f531526e6d2571b9000e73de
Nikos Graser <nikos.graser@tngtech.com>
2018-10-27 14:52:21 +0200

    Add working example with requests

--- requestsssl.py
+++ requestsssl.py
@@ -1,9 +1,21 @@
-import requests
-import ssl
 import os
+from ssl import create_default_context
 
-os.environ['SSLKEYLOGFILE'] = 'keylog.txt'
-context = ssl.create_default_context()
-resp = requests.get('https://httpbin.org/xml')
+from requests import Session
+from requests.adapters import HTTPAdapter
 
-print(resp.text)
+
+class MyHTTPAdapter(HTTPAdapter):
+    def set_ssl_context(self, ssl_context):
+        self.init_poolmanager(self._pool_connections, self._pool_maxsize,
+                              block=self._pool_block, ssl_context=ssl_context)
+
+
+os.environ.setdefault('SSLKEYLOGFILE', 'keylog.txt')
+
+session = Session()
+adapter = MyHTTPAdapter()
+adapter.set_ssl_context(create_default_context())
+session.mount('https://', adapter)
+
+session.get('https://httpbin.org/xml')
